3c1c7716e196b6a3bae89a0ab514af24e2f2dc46
wikpie <wiktor.pienko@gmail.com>
2019-11-25 10:36:08 +0100

    20.11.2019- Added charts for pulse, location now encoding here

--- app/src/main/java/com/example/opiekun/fragments/PulseFragment.kt
+++ app/src/main/java/com/example/opiekun/fragments/PulseFragment.kt
@@ -2,6 +2,7 @@ package com.example.opiekun.fragments
 
 import android.content.Context
 import android.graphics.Color
+import android.location.Geocoder
 import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
@@ -16,6 +17,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.opiekun.Communicator
 import com.example.opiekun.R
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -25,6 +27,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_pulse.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class PulseFragment : Fragment() {
     private lateinit var seniorUidd:String
@@ -37,21 +41,21 @@ class PulseFragment : Fragment() {
     private lateinit var seniorLocationNow:String
     private var nowPulse=0
     private var nowSteps=0
-    private var eightPulse=0
-    private var eightSteps=0
-    private var threePulse=0
-    private var threeSteps=0
     private val minPulse=50
     private val maxPulse=80
+    private lateinit var pulseChart:LineChart
+    private lateinit var homeChart:LineChart
     private val ref= FirebaseDatabase.getInstance().getReference("seniors")
+    private lateinit var geocoder: Geocoder
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_pulse, container, false)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val pulseChart=this.chart_pulse
-        val homeChart=this.chart_home
+        pulseChart=this.chart_pulse
+        homeChart=this.chart_home
+        geocoder=Geocoder(context, Locale.getDefault())
         val model= ViewModelProviders.of(activity!!).get(Communicator::class.java)
         model.message.observe(this, Observer<Any> { t ->
             seniorUidd=t.toString()
@@ -60,71 +64,75 @@ class PulseFragment : Fragment() {
             seniorName=t.toString()
             title.text="Wybrany senior: $seniorName"
         })
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if(seniorUidd!=" "){
-                    //te są najważniejsze
-                    seniorHome=dataSnapshot.child("$seniorUidd/dom").value.toString()
-                    nowPulse=dataSnapshot.child("$seniorUidd/now/pulse").value.toString().toInt()
-                    nowSteps=dataSnapshot.child("$seniorUidd/now/steps").value.toString().toInt()
-                    Log.d("henio_puls_teraz",nowPulse.toString())
-                    Log.d("henio_dom",seniorHome)
-                    Log.d("henio_korki_teraz",nowSteps.toString())
-                    //tu kolejno
-                    for(i in 0..23){
-                        var j=i.toString()
-                        pulseList.add(dataSnapshot.child("$seniorUidd/$j/pulse").value.toString().toInt())
-                        if((dataSnapshot.child("$seniorUidd/$i/location").value.toString()) ==seniorHome){
-                            homeList.add(0)
-                        }
-                        else homeList.add(1)
-                        Log.d("henio_puls",pulseList.toString())
-                        Log.d("henio_dom",homeList.toString())
-                    }
-                    if(nowPulse<minPulse || nowPulse>maxPulse){
-                        text_top_mid.setTextColor(Color.parseColor("#FF0000"))
-                        val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                        if (vibrator.hasVibrator()) { // Vibrator availability checking
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)) // New vibrate method for API Level 26 or higher
-                            } else {
-                                vibrator.vibrate(500) // Vibrate method for below API Level 26
-                            }
-                        }
-                    }
-                    else text_top_mid.setTextColor(Color.parseColor("#00FF00"))
-                    text_top_top.text="Teraz"
-                    text_top_mid.text="Puls: $nowPulse"
-                    text_top_bot.text="Kroki: $nowSteps"
-                    for(i in 0..23){
-                        var entriesPulse=Entry(i.toFloat(),pulseList[i].toFloat())
-                        var entriesHome=Entry(i.toFloat(),homeList[i].toFloat())
-                        entryListPulse.add(entriesPulse)
-                        entryListHome.add(entriesHome)
-                    }
-                    val dataSetPulse= LineDataSet(entryListPulse,"Wykres puls")
-                    dataSetPulse.color = Color.RED
-                    dataSetPulse.setDrawValues(false)
-                    dataSetPulse.axisDependency = YAxis.AxisDependency.LEFT
-                    val dataSetHome= LineDataSet(entryListHome,"Wykres dom")
-                    dataSetHome.color = Color.GREEN
-                    dataSetHome.setDrawValues(false)
-                    dataSetHome.axisDependency = YAxis.AxisDependency.LEFT
-                    val lineDataPulse= LineData(dataSetPulse)
-                    val lineDataHome=LineData(dataSetHome)
-                    pulseChart.data=lineDataPulse
-                    homeChart.data=lineDataHome
-                    pulseChart.invalidate()
-                    homeChart.invalidate()
-                }
 
-            }
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.w("Main", "loadPost:onCancelled", databaseError.toException()!!)
-            }
-        }
         ref.addValueEventListener(postListener)
 
 
+    }
+    private val postListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            if(seniorUidd!=" "){
+                //te są najważniejsze
+                seniorHome=dataSnapshot.child("$seniorUidd/dom").value.toString()
+                nowPulse=dataSnapshot.child("$seniorUidd/now/pulse").value.toString().toInt()
+                nowSteps=dataSnapshot.child("$seniorUidd/now/steps").value.toString().toInt()
+                Log.d("henio_puls_teraz",nowPulse.toString())
+                Log.d("henio_dom",seniorHome)
+                Log.d("henio_korki_teraz",nowSteps.toString())
+                //tu kolejno
+                for(i in 0..23){
+                    var j=i.toString()
+                    pulseList.add(dataSnapshot.child("$seniorUidd/$j/pulse").value.toString().toInt())
+                    var latitude=dataSnapshot.child("$seniorUidd/$i/latitude").value.toString().toDouble()
+                    var longitude=dataSnapshot.child("$seniorUidd/$i/longitude").value.toString().toDouble()
+                    var addresses=geocoder.getFromLocation(latitude,longitude,1)
+                    if(addresses[0].getAddressLine(0) ==seniorHome){
+                        homeList.add(0)
+                    }
+                    else homeList.add(1)
+                    Log.d("henio_puls",pulseList.toString())
+                    Log.d("henio_dom",homeList.toString())
+                }
+                if(nowPulse<minPulse || nowPulse>maxPulse){
+                    text_top_mid.setTextColor(Color.parseColor("#FF0000"))
+                    val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                    if (vibrator.hasVibrator()) { // Vibrator availability checking
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)) // New vibrate method for API Level 26 or higher
+                        } else {
+                            vibrator.vibrate(500) // Vibrate method for below API Level 26
+                        }
+                    }
+                }
+                else text_top_mid.setTextColor(Color.parseColor("#00FF00"))
+                text_top_top.text="Teraz"
+                text_top_mid.text="Puls: $nowPulse"
+                text_top_bot.text="Kroki: $nowSteps"
+                for(i in 0..23){
+                    var entriesPulse=Entry(i.toFloat(),pulseList[i].toFloat())
+                    var entriesHome=Entry(i.toFloat(),homeList[i].toFloat())
+                    entryListPulse.add(entriesPulse)
+                    entryListHome.add(entriesHome)
+                }
+                val dataSetPulse= LineDataSet(entryListPulse,"Wykres puls")
+                dataSetPulse.color = Color.RED
+                dataSetPulse.setDrawValues(false)
+                dataSetPulse.axisDependency = YAxis.AxisDependency.LEFT
+                val dataSetHome= LineDataSet(entryListHome,"Wykres dom")
+                dataSetHome.color = Color.GREEN
+                dataSetHome.setDrawValues(false)
+                dataSetHome.axisDependency = YAxis.AxisDependency.LEFT
+                val lineDataPulse= LineData(dataSetPulse)
+                val lineDataHome=LineData(dataSetHome)
+                pulseChart.data=lineDataPulse
+                homeChart.data=lineDataHome
+                pulseChart.invalidate()
+                homeChart.invalidate()
+            }
+
+        }
+        override fun onCancelled(databaseError: DatabaseError) {
+            Log.w("Main", "loadPost:onCancelled", databaseError.toException()!!)
+        }
     }
 }
